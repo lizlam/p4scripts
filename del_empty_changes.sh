65c3ead1138c7895c6ed30611bfbfba674bb8977
Liz Lam <llam@perforce.com>
2013-10-25 11:12:40 -0700

    Adding README.md and script to delete empty changelists.

---
+++ del_empty_changes.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# Script to delete empy changelists
+# Set $P4PORT and $P4USER environment variables
+
+if [ $# -eq 0 ]
+then
+    echo "usage: del_empty_changes.sh [client]"
+    exit 1
+fi
+
+i=0
+num=`p4 changes -s pending -c $1 | wc -l`
+num_deleted=0
+eval array=( $(p4 -ztag changes -s pending -c $1 | grep "change " | awk '{print $3}' ))
+
+function is_empty() {
+    file_result=`p4 -ztag describe $1 | grep depotFile`
+    if [ "$file_result" != "" ]
+    then
+        file_result_count=`expr $file_result_count + 1`   
+    fi
+}
+
+function check_fixes() {
+    fixes_result=`p4 -ztag describe $1 | grep job`
+    if [ "$fixes_result" != "" ]
+    then
+        fixes_result_count=`expr $fixes_result_count + 1`
+        change_with_fix=$1
+    fi
+} 
+
+function check_shelved() {
+    shelved_result=`p4 -ztag describe -s $1 | grep shelved`
+    if [ "$shelved_result" != "" ]
+    then
+        shelved_result_count=`expr $shelved_result_count + 1`
+    fi
+}
+
+function summarize() {
+    echo "-----------S U M M A R Y--------------"
+    echo "Total # of pending changelists: `expr $num + 1` "
+    echo "# of changelists deleted: $num_deleted"
+    echo "# of empty changelists with fixes: $fixes_result_count"
+    echo "# of empty changelists with shelved files: $shelved_result_count"    
+    echo "# of changelists with open files: $file_result_count"
+}
+
+
+while [ $i -lt $num ]
+do
+    current_change=${array[$i]}
+    is_empty $current_change
+    if [ "$file_result" = "" ]
+    then
+        check_shelved $current_change
+        check_fixes $current_change
+        p4 -c $1 change -d $current_change
+        # Need the extra check since unsuccessful delete of empty
+        # changelist with fix returns 0 status
+        if [ $? -eq 0 -a "$current_change" != "$change_with_fix" ]
+        then
+            num_deleted=`expr $num_deleted + 1`
+        fi
+    else
+        echo "Change $current_change is not empty."
+    fi
+    i=`expr $i + 1`
+done
+summarize
