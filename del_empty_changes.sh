97a215bc4b24077710ddaa37bf1e918c142784ad
Liz <liz_lam_217@yahoo.com>
2014-02-11 22:40:11 -0800

    Update del_empty_changes.sh
    Fixed typo and added comments.

--- del_empty_changes.sh
+++ del_empty_changes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script to delete empy changelists
+# Script to delete empty changelists
 # Set $P4PORT and $P4USER environment variables
 
 if [ $# -eq 0 ]
@@ -13,6 +13,8 @@ num=`p4 changes -s pending -c $1 | wc -l`
 num_deleted=0
 eval array=( $(p4 -ztag changes -s pending -c $1 | grep "change " | awk '{print $3}' ))
 
+# Function to check if changelist contains open files. 
+# If changelist contains open files, increment $file_result_count
 function is_empty() {
     file_result=`p4 -ztag describe $1 | grep depotFile`
     if [ "$file_result" != "" ]
@@ -21,6 +23,8 @@ function is_empty() {
     fi
 }
 
+# Function to check if changelist contains fixes.
+# If changelist contains fixes, increment $fixes_result_count
 function check_fixes() {
     fixes_result=`p4 -ztag describe $1 | grep job`
     if [ "$fixes_result" != "" ]
@@ -30,6 +34,8 @@ function check_fixes() {
     fi
 } 
 
+# Function to check if changelist contains shelved files.
+# If changelist contains shelved files, increment $shelved_result_count
 function check_shelved() {
     shelved_result=`p4 -ztag describe -s $1 | grep shelved`
     if [ "$shelved_result" != "" ]
